9b6408f630d4dbdbd87282cfb6cb8177bf996158
Ming Zhou <kornbergfresnel@outlook.com>
2022-08-14 11:28:03 +0800

    update

--- dmc_mpo.py
+++ dmc_mpo.py
@@ -21,35 +21,33 @@ from mpo import MPOPolicy
 
 D4RL_DATASET = pathlib.Path.home() / ".d4rl/datasets"
 
-parser = argparse.ArgumentParser(description="Run training")
-parser.add_argument("--task", type=str, required=True)
-parser.add_argument("--debug", action="store_true")
-parser.add_argument("--seed", type=int, default=0)
-parser.add_argument("--buffer-size", type=int, default=1000000)
-parser.add_argument("--hidden-sizes", type=int, nargs="*", default=[256, 256])
-parser.add_argument("--actor-lr", type=float, default=1e-3)
-parser.add_argument("--critic-lr", type=float, default=1e-3)
-parser.add_argument("--gamma", type=float, default=0.99)
-parser.add_argument("--tau", type=float, default=0.005)
-parser.add_argument("--exploration-noise", type=float, default=0.1)
-parser.add_argument("--start-timesteps", type=int, default=25000)
-parser.add_argument("--epoch", type=int, default=200)
-parser.add_argument("--step-per-epoch", type=int, default=5000)
-parser.add_argument("--step-per-collect", type=int, default=1)
-parser.add_argument("--update-per-step", type=int, default=1)
-parser.add_argument("--n-step", type=int, default=1)
-parser.add_argument("--batch-size", type=int, default=256)
-parser.add_argument("--training-num", type=int, default=1)
-parser.add_argument("--test-num", type=int, default=10)
-parser.add_argument("--logdir", type=str, default="log")
-parser.add_argument("--render", type=float, default=0.0)
-parser.add_argument(
-    "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
-)
-parser.add_argument("--wandb-project", type=str, default="dmc.benchmarking")
-
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run training")
+    parser.add_argument("--task", type=str, required=True)
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--buffer-size", type=int, default=1000000)
+    parser.add_argument("--hidden-sizes", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--actor-lr", type=float, default=1e-3)
+    parser.add_argument("--critic-lr", type=float, default=1e-3)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--exploration-noise", type=float, default=0.1)
+    parser.add_argument("--start-timesteps", type=int, default=25000)
+    parser.add_argument("--epoch", type=int, default=200)
+    parser.add_argument("--step-per-epoch", type=int, default=5000)
+    parser.add_argument("--step-per-collect", type=int, default=1)
+    parser.add_argument("--update-per-step", type=int, default=1)
+    parser.add_argument("--n-step", type=int, default=1)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--training-num", type=int, default=1)
+    parser.add_argument("--test-num", type=int, default=10)
+    parser.add_argument("--logdir", type=str, default="log")
+    parser.add_argument("--render", type=float, default=0.0)
+    parser.add_argument(
+        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
+    )
     args = parser.parse_args()
 
     env, train_envs, test_envs = make_dmc_env(
--- mpo.py
+++ mpo.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
-from turtle import forward
-from typing import Any, Dict, List, Type, Optional, Union
+from typing import Any, Dict, List, Type, Optional, Union, Tuple
 
 import warnings
 import torch
@@ -19,10 +18,10 @@ from tianshou.utils.net.continuous import Actor as ContinuousActor
 from tianshou.utils.net.discrete import Actor as DiscreteActor
 
 
-def _dual(target_q: np.ndarray, eta: float, epsilon: float):
+def _dual(eta: np.ndarray, target_q: np.ndarray, epsilon: float):
     """Dual function of the non-parametric variational
 
-    g(eta) = eta * dual_constraint + eta \sum \log(\sum\exp(Q(s,a)/eta))
+    g(eta) = eta * dual_constraint + eta sum{log(sum{exp(Q(s,a)/eta)})}
     """
 
     max_q = np.max(target_q, -1)
@@ -54,42 +53,6 @@ def btr(m):
 
 def bt(m):
     return m.transpose(dim0=-2, dim1=-1)
-
-
-def gaussian_kl(mu_i, mu, Ai, A):
-    """
-    decoupled KL between two multivariate gaussian distribution
-    C_mu = KL(f(x|mu_i,sigma_i)||f(x|mu,sigma_i))
-    C_sigma = KL(f(x|mu_i,sigma_i)||f(x|mu_i,sigma))
-    :param mu_i: (B, n)
-    :param mu: (B, n)
-    :param Ai: (B, n, n)
-    :param A: (B, n, n)
-    :return: C_mu, C_sigma: scalar
-        mean and covariance terms of the KL
-    :return: mean of determinanats of sigma_i, sigma
-    """
-    n = A.size(-1)
-    mu_i = mu_i.unsqueeze(-1)  # (B, n, 1)
-    mu = mu.unsqueeze(-1)  # (B, n, 1)
-    sigma_i = Ai @ bt(Ai)  # (B, n, n)
-    sigma = A @ bt(A)  # (B, n, n)
-    sigma_i_det = sigma_i.det()  # (B,)
-    sigma_det = sigma.det()  # (B,)
-    sigma_i_det = torch.clamp_min(sigma_i_det, 1e-6)
-    sigma_det = torch.clamp_min(sigma_det, 1e-6)
-    sigma_i_inv = sigma_i.inverse()  # (B, n, n)
-    sigma_inv = sigma.inverse()  # (B, n, n)
-
-    inner_mu = (
-        (mu - mu_i).transpose(-2, -1) @ sigma_i_inv @ (mu - mu_i)
-    ).squeeze()  # (B,)
-    inner_sigma = (
-        torch.log(sigma_det / sigma_i_det) - n + btr(sigma_inv @ sigma_i)
-    )  # (B,)
-    C_mu = 0.5 * torch.mean(inner_mu)
-    C_sigma = 0.5 * torch.mean(inner_sigma)
-    return C_mu, C_sigma, torch.mean(sigma_i_det), torch.mean(sigma_det)
 
 
 class MPOPolicy(BasePolicy):
@@ -256,14 +219,17 @@ class MPOPolicy(BasePolicy):
                 sampled_next_actions.reshape(-1, self._act_dim),
             )
             next_state_values = next_q_values.reshape(batch_size, -1).mean(-1)
-
-            y = batch.rew + self._gamma * next_state_values
+            assert batch.rew.shape == next_state_values.shape, (
+                batch.rew.shape,
+                next_state_values.shape,
+            )
+            y = batch.rew.float() + self._gamma * next_state_values
 
         self.critic_optim.zero_grad()
         q_values = self.critic(batch.obs, batch.act).squeeze()
         loss = self._norm_critic_loss(q_values, y)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.criti.parameters(), self._critic_grad_norm)
+        torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self._critic_grad_norm)
         self.critic_optim.step()
         return loss, y
 
@@ -292,40 +258,44 @@ class MPOPolicy(BasePolicy):
                 particle_num, batch_size
             )  # (K, batch_size)
             target_q_np = target_q.cpu().transpose(0, 1).numpy()  # (batch_size, K)
+            reference_log_prob = reference_policy.expand(
+                (particle_num, batch_size)
+            ).log_prob(sampled_actions)
 
         self._eta = minimize(
             _dual,
-            target_q_np,
-            self._eta,
-            self._epsilon,
+            np.asarray([self._eta]),
+            args=(target_q_np, self._epsilon),
             method="SLSQP",
             bounds=[(1e-6, None)],
         ).x[0]
 
         # M-step: update actor based on Lagrangian
         average_actor_loss = 0.0
+        average_kl = 0.0
         # normalize q
         norm_target_q = F.softmax(target_q / self._eta, dim=0)  # (K, batch_size)
         for _ in range(self._mstep_iter_num):
             logits, _ = self.actor(batch.obs)
             policy = self.dist_fn(logits)
-            mle_loss = torch.mean(
-                norm_target_q
-                * policy.expand((particle_num, batch_size)).log_prob(sampled_actions)
-            )  # (K, batch_size)
-            kl_to_ref_policy = torch.distributions.kl.kl_divergence(
-                policy.probs, reference_policy.probs
+            log_prob = policy.expand((particle_num, batch_size)).log_prob(
+                sampled_actions
             )
+            mle_loss = torch.mean(norm_target_q * log_prob)  # (K, batch_size)
+
+            # compute KL divergence
+            kl_to_ref_policy = F.kl_div(log_prob, reference_log_prob)
+            average_kl += kl_to_ref_policy.item() / self._mstep_iter_num
 
             # Update lagrange multipliers by gradient descent
-            self._eta_kl -= (
-                self._alpha * (self._eta_kl - kl_to_ref_policy).detach().item()
-            )
+            self._eta_kl -= self._alpha * (self._eta_kl - kl_to_ref_policy.item())
             self._eta_kl = max(self._eta_kl, 0.0)
 
             self.actor_optim.zero_grad()
 
-            actor_loss = -(mle_loss + self._eta_kl * (self._epsilon - kl_to_ref_policy))
+            actor_loss = -(
+                mle_loss + self._eta_kl * (self._epsilon - kl_to_ref_policy.mean())
+            )
             actor_loss.backward()
             torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 5.0)
 
@@ -338,6 +308,7 @@ class MPOPolicy(BasePolicy):
             "loss/actor": average_actor_loss,
             "loss/critic": critic_loss.item(),
             "est/q": mean_est_q.item(),
+            "est/kl_to_ref": average_kl,
         }
 
     def exploration_noise(
@@ -349,5 +320,6 @@ class MPOPolicy(BasePolicy):
             act = act + self._noise(act.shape)
             if not self._discrete_act:
                 act = np.clip(act, a_min=-1.0, a_max=1.0)
+            return act
         warnings.warn("Cannot add exploration noise to non-numpy_array action.")
         return act
